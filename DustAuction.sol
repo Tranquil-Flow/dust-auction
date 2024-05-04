e3d5bed30d3dbaac59fd9df5a69768124a62eab6
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-05-04 13:39:56 +1000

    init simple DustAuction.sol

---
+++ DustAuction.sol
@@ -0,0 +1,127 @@
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+//TODO: Add Events
+//TODO: Add token being accepted for offers
+//TODO: Cleanup variable names to be unique
+
+contract DustAuction is ReentrancyGuard {
+
+    // Errors
+    error InvalidTokenAmount(uint256 amount);
+    error InvalidTokenAddress(address addr);
+    error TokenTransferFailed();
+    error OfferInvalid();
+    error NotOwnerOfOffer();
+
+    // State Variables
+    struct Offer {
+        uint offerID;
+        address seller;
+        address tokenSelling;
+        uint tokenAmount;
+        uint tradeOfferStartTime;
+        uint timeline;
+        bool offerOpen;
+    }
+
+    Offer[] public offers;
+
+
+    /**
+    * @notice Make an offer to sell an asset.
+    * @param tokenSelling The contract address of the token being sold.
+    * @param tokenBuying The contract address of the token being bought.
+    * @param tokenAmount The amount of tokens being sold.
+    * @param timeline The amount of time in seconds for ###minimum sell price to be achieved###.
+    */
+    function makeOffer(
+        address tokenSelling,
+        address tokenBuying,
+        uint tokenAmount,
+        uint timeline
+    ) external reentrancyGuard returns (uint) {
+        if (tokenAmount <= 0) {revert InvalidTokenAmount(tokenAmount);}
+        if (tokenSelling == address(0)) {revert InvalidTokenAddress(tokenSelling);}
+
+        // Approve the contract to spend the tokens
+        bool approvalSuccess = IERC20(tokenSelling).approve(address(this), tokenAmount);
+        if (!approvalSuccess) {revert TokenApprovalFailed();}
+
+        // Transfer tokens to contract
+        bool transferSuccess = IERC20(tokenSelling).transferFrom(msg.sender, address(this), tokenAmount);
+        if (!transferSuccess) {revert TokenTransferFailed();}
+
+        // Store the offer for others to view
+        offers.push(Offer(offers.length, msg.sender, tokenSelling, tokenAmount, block.timestamp, timeline, true));
+
+        return offers.length - 1;
+    }
+
+    // Given an input asset amount, returns the output amount of the other asset at current time.
+    function getAmountOut(uint offerID, uint inputAmount) public returns (uint) {
+
+    }
+
+    // Returns the input amount required to buy the given output asset amount at current time.
+    function getAmountIn(uint offerID) public returns (uint) {
+
+    }
+
+    function acceptOfferPartial(
+        uint offerID,
+        uint inputAmount
+    ) external reentrancyGuard {
+        if (offers[offerID].offerOpen == false) {revert OfferInvalid();}
+        if (inputAmount <= 0) {revert InvalidTokenAmount(inputAmount);}
+
+        // Check the amount of tokens the buyer will receive for inputAmount and at current time
+        uint amountReceived = getAmountOut(offerID, inputAmount);
+
+        // Update the offer
+        offers[offerID].tokenAmount -= amountReceived;
+
+        // Transfer tokens to seller
+        IERC20(offers[offerID].tokenBuying).transfer(offers[offerID].seller, inputAmount);
+
+        // Transfer tokens to buyer
+        IERC20(offers[offerID].tokenSelling).transfer(msg.sender, amountReceived);
+
+    }
+
+    function acceptOfferFull(
+        uint offerID
+    ) external reentrancyGuard {
+        if (offers[offerID].offerOpen == false) {revert OfferInvalid();}
+
+        // Check the amount of tokens needed to buy the offer at current time
+        uint amountNeeded = getAmountIn(offerID);
+
+        // Close the offer
+        offers.offerID.offerOpen = false;
+
+        // Transfer tokens to seller
+        IERC20(offers[offerID].tokenBuying).transfer(offers[offerID].seller, amountNeeded);
+
+        // Transfer tokens to buyer
+        IERC20(offers[offerID].tokenSelling).transfer(msg.sender, offers[offerID].tokenAmount);
+
+    }
+
+    function cancelOffer(
+        uint offerID
+    ) external reentrancyGuard {
+        if (offers[offerID].seller != msg.sender) {revert NotOwnerOfOffer();}
+        if (offers[offerID].offerOpen == false) {revert OfferInvalid();}
+
+        // Close the offer
+        offers.offerID.offerOpen = false;
+
+        // Transfer tokens back to seller
+        bool transferSuccess = IERC20(offers[offerID].tokenSelling).transfer(offers[offerID].seller, offers[offerID].tokenAmount);
+        if (!transferSuccess) {revert TokenTransferFailed();}
+    }
+
+}
